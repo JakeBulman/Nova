4772278a0ea52beba3d7d64b2b0d3b8279a4dd78
Jake Bulman <jakebulman@hotmail.com>
2023-08-08 22:02:38 +0100

    Scaled Marks JUSCHE data load

--- enquiries/script_ServerDeltaLoad.py
+++ enquiries/script_ServerDeltaLoad.py
@@ -23,7 +23,7 @@ else:
 
 django.setup()
 
-from enquiries.models import CentreEnquiryRequests, EnquiryRequestParts, EnquiryComponents, EnquiryPersonnel, EnquiryPersonnelDetails, EnquiryBatches, EnquiryComponentElements, TaskManager, UniqueCreditor, EnquiryComponentsHistory, EnquiryComponentsExaminerChecks, TaskTypes, EarServerSettings, EnquiryGrades, EnquiryDeadline, ExaminerPanels, MarkTolerances
+from enquiries.models import CentreEnquiryRequests, EnquiryRequestParts, EnquiryComponents, EnquiryPersonnel, EnquiryPersonnelDetails, EnquiryBatches, EnquiryComponentElements, TaskManager, UniqueCreditor, EnquiryComponentsHistory, EnquiryComponentsExaminerChecks, TaskTypes, EarServerSettings, EnquiryGrades, EnquiryDeadline, ExaminerPanels, MarkTolerances, ScaledMarks
 
 def load_core_tables():
 
@@ -794,6 +794,63 @@ def load_core_tables():
                 eps_com_id = row[1],
                 mark_tolerance = row[2]
             )
+        
+        # # Get datalake data - Enquiry Request Parts
+    with pyodbc.connect("DSN=hive.ucles.internal", autocommit=True) as conn:
+        df = pd.read_sql(f'''
+        select 
+            c.sessionassessmentcomponentid,
+            a.assessmentcode as eps_ass_code,
+            a.componentid as eps_com_id,
+            c.centrenumber as eps_cnu_id,
+            c.candidatenumber as eps_cand_no,
+            c.mark as raw_mark,
+            c.assessormark as assessor_mark,
+            c.finalmark as final_mark,
+            c.examinernumber as exm_examiner_no,
+            case
+            when c.assessormark is not null then assessormark 
+            else c.mark
+            end as scaled_mark,
+            case
+            when assessormark is not null then "Scaled" 
+            when assessormark is null then "No scaling"
+            end as original_exm_scaled
+        from cie.ods_CandidateMarkElementMarks as c
+        left join cie.ods_sessionassessmentcomponents as s
+        on c.sessionassessmentcomponentid=s.sessionassessmentcomponentid
+        and c.sessionpartitionkey=s.sessionpartitionkey
+        left join cie.ods_assessmentcomponents as a
+        on s.assessmentcomponentid=a.assessmentcomponentid
+        where c.businessstreamid='02'
+            and s.isdeletedfromsource!=1
+            and c.isdeletedfromsource!=1
+            and a.isdeletedfromsource!=1
+            and c.examinernumber!=''
+            and s.sessionid in ({session_id}) 
+                                ''', conn)
+
+    def insert_to_model_erp(row):
+        try:
+            ScaledMarks.objects.create(
+                eps_ass_code = row['eps_ass_code'],
+                eps_com_id = row['eps_com_id'],
+                eps_cnu_id = row['eps_cnu_id'],
+                eps_cand_no = row['eps_cand_no'],
+                raw_mark = row['raw_mark'],
+                assessor_mark  = row['assessor_mark'],
+                final_mark = row['final_mark'],
+                exm_examiner_no = row['exm_examiner_no'],
+                scaled_mark = row['scaled_mark'],
+                original_exm_scaled = row['original_exm_scaled'],
+                )
+        except:
+            pass
+
+    df.apply(insert_to_model_erp, axis=1)
+
+
+    print("ERP loaded:" + str(datetime.datetime.now()))
 
 
     end_time = datetime.datetime.now()
