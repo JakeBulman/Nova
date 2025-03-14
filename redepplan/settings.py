a1bb7b90b1c3460e9519e6a7eda6e5b8178aca45
JakeBulman <jakebulman@hotmail.com>
2025-03-14 14:16:40 +0000

    Compress settings

--- redepplan/settings.py
+++ redepplan/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static').replace('\\', '/'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 's3earnova/UAT/media')
 
-COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_ROOT = STATIC_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
