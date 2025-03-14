79b0210cd168296843b7a9bec20093f304109969
JakeBulman <jakebulman@hotmail.com>
2025-03-14 14:31:05 +0000

    settings

--- redepplan/settings.py
+++ redepplan/settings.py
@@ -150,12 +150,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 's3earnova/UAT/static').replace('\\', '/')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/s3earnova/UAT/static').replace('\\', '/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static').replace('\\', '/'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 's3earnova/UAT/media').replace('\\', '/')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/s3earnova/UAT/media').replace('\\', '/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
