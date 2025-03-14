e5def4dc007e3b66790500c3e6f21f162010afb6
JakeBulman <jakebulman@hotmail.com>
2025-03-14 14:25:35 +0000

    Compress settings

--- redepplan/settings.py
+++ redepplan/settings.py
@@ -151,20 +151,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 's3earnova/UAT/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 's3earnova/UAT/static').replace('\\', '/')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static').replace('\\', '/'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 's3earnova/UAT/media')
-
-COMPRESS_ROOT = STATIC_URL
+MEDIA_ROOT = os.path.join(BASE_DIR, 's3earnova/UAT/media').replace('\\', '/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 
 
 INSTALLED_APPS += (
